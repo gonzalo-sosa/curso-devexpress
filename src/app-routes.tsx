a558862dc66e0336300c06f3bf00546d5219fb20
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-03-16 11:26:10 -0300

    Format

--- src/app-routes.tsx
+++ src/app-routes.tsx
@@ -1,54 +1,53 @@
 import { withNavigationWatcher } from "./contexts/navigation";
 import {
-	AboutPage,
-	ChartPage,
-	DatagridPage,
-	FileUploaderPage,
-	FormPage,
-	HomePage,
-	ProfilePage,
-	TasksPage,
+  AboutPage,
+  ChartPage,
+  DatagridPage,
+  FileUploaderPage,
+  FormPage,
+  HomePage,
+  ProfilePage,
+  TasksPage,
 } from "./pages";
 
 const routes = [
-	{
-		path: "/tasks",
-		element: TasksPage,
-	},
-	{
-		path: "/profile",
-		element: ProfilePage,
-	},
-	{
-		path: "/home",
-		element: HomePage,
-	},
-	{
-		path: "/about",
-		element: AboutPage,
-	},
-	{
-		path: "/chart",
-		element: ChartPage,
-	},
-	{
-		path: "/datagrid",
-		element: DatagridPage,
-	},
-	{
-		path: "/form",
-		element: FormPage,
-	},
-	,
-	{
-		path: "/file-uploader",
-		element: FileUploaderPage,
-	},
+  {
+    path: "/tasks",
+    element: TasksPage,
+  },
+  {
+    path: "/profile",
+    element: ProfilePage,
+  },
+  {
+    path: "/home",
+    element: HomePage,
+  },
+  {
+    path: "/about",
+    element: AboutPage,
+  },
+  {
+    path: "/chart",
+    element: ChartPage,
+  },
+  {
+    path: "/datagrid",
+    element: DatagridPage,
+  },
+  {
+    path: "/form",
+    element: FormPage,
+  },
+  {
+    path: "/file-uploader",
+    element: FileUploaderPage,
+  },
 ];
 
 export default routes.map((route) => {
-	return {
-		...route,
-		element: withNavigationWatcher(route.element, route.path),
-	};
+  return {
+    ...route,
+    element: withNavigationWatcher(route.element, route.path),
+  };
 });
